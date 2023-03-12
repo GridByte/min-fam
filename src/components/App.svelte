094acaa3a97458f81a71f2f11926e4f50e13ac3f
Gaute Aasmo <grid@Gautes-MBP-2.home>
2023-03-13 00:38:04 +0100

    first commit

---
+++ src/components/App.svelte
@@ -0,0 +1,57 @@
+
+<script>  
+  import MainView from './MainView.svelte';
+</script>
+
+<main>
+  <div class="root__navbar"> 
+    <div class="top-text">
+      Gaute Aasmo 3STH - Min familie
+    </div>
+  </div>
+  <div class="root__sidebar">
+    
+  </div>
+  <div class="root__main_view">
+    <MainView/>
+  </div>
+</main>
+
+<style>
+  main {
+    width: 100%;
+    height: 100%;
+    padding: 0;
+    display: grid;
+    grid-template-areas: 
+    "navbar navbar"
+    "sidebar main_view";
+    grid-template-columns: 0px 200fr;
+		grid-template-rows: 75px 1fr;
+  }
+
+  .root__navbar {
+    grid-area: navbar;
+    background-color: rgb(24, 24, 24);
+  }
+
+  .root__sidebar {
+    grid-area: sidebar;
+    background-color: green;
+  }
+  
+  .root__main_view {
+    grid-area: main_view;
+    background-color: rgb(75, 80, 75);
+  }
+
+  .top-text {
+    color: white;
+    opacity: 1;
+    widows: 100%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+</style>
---
+++ src/components/MainView.svelte
@@ -0,0 +1,247 @@
+
+
+
+
+<div class="content">
+    <div class="top-container">
+        <img src="./nordbanen.jpeg" alt="Can't load" class="full-img">
+        <h1>Hvordan var det å bygge Nordlandsbanen?</h1>
+    </div>
+    <section>
+        <div class="text-container">
+            <h2>Min oldefar</h2>
+            Øistein Orre Møller er min oldefar, og far til min Mormor. Han ble født 1. November 1920 og vokste opp på Vega, en stor øy ikke så langt fra Mosjøen. Der bodde han på en gård som het Vegmoen sammen med sine foreldre, Torstein Møller og Anna Johannessen, og sine søsken, Anders, Aase, Sigurd og Harald. På denne tiden var populasjonen på Vega vesentlig større enn det den er i dag (omtrent 3000 innbyggere i 1920 og 1200 i dag), og både skole og helsetjeneste var godt etablert. Ifølge intervju med Karin Humstad, min mormor, var Øistein dyktig på skolen. Han startet på skolen da han var seks år gammel og fikk gode karakterer, dessuten var han flink til å tegne, og var designeren bak mange av dekortegningene på skolen. Som ungdom drev han med lofotfisking, noe han dessverre ikke kunne fortsette med på grunn av en alvorlig ørebetennelse, og senere begynte han på fylkesskolen i Mosjøen. Da han returnerte til Vega i en alder på 22 til 24 år, giftet han seg med Jorunn møller, min oldemor, og etablert seg i nærheten av Vegmoen hvor han hadde vokst opp. Her fikk Øistein og Jorunn en datter, Karin, som nå er min mormor. Etter å ha fått jobb i Vegvesenet, noe som var sesongarbeid, startet han arbeidet på Nordlandsbanen. Her fulgte han sin fars 
+            <q>togspor</q> 
+            og holdt blant annet på med dynamittsprengning av grunnfjell og tunneler som skytebas.
+
+        </div>
+        <div class="img-container" style="width: 300px">
+            <img src="./portrett_øistein.jpg" alt="Can't load">
+            <div class="img-text">
+                Øystein Orre Møller som ung man (sannsynligvis 25 år)
+            </div>
+        </div>
+    </section>
+    <div class="video-section">
+        <h2>Kort video om Nordlandsbanen</h2>
+        <div class="video-box">
+            <iframe width="100%" height="100%" src="https://www.youtube.com/embed/B2nKOQPqROs" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        </div>
+    </div>
+    <section>
+        <div class="text-container">
+            <h2>Øisteins arbeid på Nordlandsbanen</h2>
+            <p>Nordlandsbanen begynte i 1902 på Hell, som ligger sør for Trondheim Lufthavn, og har siden den gang blitt etappevis forlenget helt opp til Bodø i 1962. I løpet av drøyt 60 år med konstruksjonsarbeid og ombygging har arbeidsforholdene på banen variert mye, blant annet på grunn av okkupasjonen under 2. verdenskrig. Da Øistein begynte å jobbe på Nordlandsbanen som ung voksen, var heldigvis krigen over, og NSB kunne drive konstruksjonsarbeidet selvstendig. Arbeidet til Øistein foregikk rundt omkring vefsnområdet, som ligger 6 til 10 mil fra Vega, og han måtte hovedsakelig jobbe om våren, sommeren og høsten. Til tross for snø og frost, kan det godt være at han arbeidet om vinteren også. Dette er fordi han blant annet drev med tunnelsprengning som skytebas, og kunne derfor oppholde seg inne i grunnfjellet hvor temperaturen var høyere og forholdene bedre enn utenfor.</p>
+            Da Øistein først returnerte hjem på ferie, ble han værende bare en uke om gangen, og når han returnerte om vinteren, dro han ofte opp Nord på lofotfisket. Både på grunn av lofotfisket og jernbanearbeidet, tilbragte ikke Øistein mye tid sammen med mormor samt kjernefamilien, og han var ikke alene. I boka «Jernbanefolket forteller» forteller noen av datidens jernbanearbeidere om en vanskelig hussituasjon i Nord-Norge, og om brakkelivet langs anleggene. En av dem som det står om i boka, anleggsarbeider Ole Villman, kommenterte dette om brakkelivet: «Det var bra nok, men ikke det samme som å bo hjemme med sine egne». Hvis hussituasjonene hadde vært bedre for Øistein, hadde det kanskje vært mulig for hele familien å flytte nærmere jernbaneanleggene, og de kunne derfor tilbrakt mer tid sammen. Dessverre for Øistein og mange andre jernbanearbeidere, måtte de bo i såkalte brakkebyer utenfor jernbanevirksomheten. Han fikk derfor ikke vært mye sammen med familien sin, men på den andre siden tjente ham dem nokså godt.
+            <blockquote><q>Penger var ikke et problem, og vi fikk alt vi trengte. Vi var ikke rike, men hadde nok til å klare oss.</q> – Karin Humstad (mormor)</blockquote>
+        </div>
+        <div class="img-container" style="width: 300px;">
+            <img src="./nordlandsposten.png" alt="Can't load">
+            <div class="img-text">
+                Forsiden på Nordlandsposten (desember 1947). Legg merke til overskriftene «FJELLETS MAKT ER BRUTT» og «All kraft må settes inn på Dunderlandsbanens ombygging» (Dunderlandsbanen er altså en del av Nordlandsbanen). Å bygge en jernbane krever arbeid, men vesentlig mer arbeid er det å bygge en jernbane gjennom Norges fjorder og fjell. På dunderlandsbanen var maksimal hastighet knappe 40 km/t fordi banen var dårlig sikret under krigen, og det gjenstod masse arbeid i form av ombygging.
+            </div>
+            <img src="./jernbanefolk_forteller.png" alt="Can't load">
+            <div class="img-text">
+                Fra bokomslaget til Jernbanefolk forteller av Kaare Wilhelmsen (1977). I boka forteller jernbanefolk fra alle kanter av Jernbane-Norge om sitt arbeid gjennom tidene.
+            </div>
+        </div>
+    </section>
+    <section>
+        <div class="img-container" style="width: 600px; margin: 50px 0 0 0;" >
+            <img src="./reklameboken_2.png" alt="Can't load">
+            <div class="img-text">
+                Hentet fra Reklameboken for jernbanen i Nord-Norge (1953) (se kildeliste). Ifølge intervju med mormor var ikke Tyskerne et vanlig tema rundt middagsbordet da Øistein var liten. NSB oppfordret innad taushet ettersom jernbanearbeidet ble ansett som militært under krigen.             </div>
+        </div>
+    </section>
+    <div class="text-section">
+        Arbeidsdagene var lange og harde, men arbeidet var ordentlig og NSB brøt ikke med menneskerettighetene. Livet i brakkebyene var enkelt - her spiste de, sov og gjorde av seg ellers, og de førte arbeidsbøker av god kvalitet hvor blant annet antall arbeidstimer ble nedskrevet. I desember 1947 strakte Nordlandsbanen seg allerede opp til Lønsdal, forbi vefsenområdet der Øistein hovedsakelig var stasjonert, og sjansen er derfor stor for at han drev med sikring og ombygging av banen. Dette var en viktig jobb fordi forholdene under krigen og den forserte jernbaneutbyggingen nordover for Mosjøen medførte en dårlig standard på fyllinger (jord og steinmasser) og på banelegemet på Nordlandsbanen.
+    </div>
+    <section>
+        <div class="img-container" style="width: 300px;">
+            <img src="./portrett_krigsfange.jpeg" alt="Can't load">
+            <div class="img-text">
+                Navnløs krigsfange (1945) fotografert av Leiv Kreyberg. «De var ikke vandt til at besøkende brakte noe godt med seg».
+            </div>
+        </div>
+        <div class="text-container">
+            <h2>En mørk fortid</h2>
+            I 1941, etter at tyskerne hadde okkupert Norge, begynte anleggsarbeidet å komme opp i et voldsomt tempo. Årsakene til den raske fremdriften var blant annet en enorm arbeidsstyrke på 26 000 mann, hvorav 16 000 av disse drev med jernbanearbeid mellom Mo i rana og Fauske, og bestod vesentlig av krigsfanger. Til sammenlikning var det knappe 800 mann som jobbet på strekningen Mo i rana til Bodø i 1955 (studieselsapets årsmøte. Se kildeliste). Av disse krigsfangene var omtrent 5 000 sovjetiske, mens resten var for det meste nordmenn og polakker. Den Tyske halvorganisasjonen OT (Organisation Todt), som drev med bygg- og anleggsvirksomhet, krevde 10 000 nordmenn, hvorav disse var fra Østlandet og ble stasjonert på Nordlandsbanen.
+            <blockquote><q>«OT valgte folk på Østlandet med vilje. Da fikk de sendt dem lengst mulig unna der de bodde. Slik ble det vanskeligere å flykte»</q> - Gunnar D. Hatlehol (klarttale.no, se kildeliste)</blockquote>
+        </div>
+    </section>
+    <div class="text-section">
+        De forferdelige forholdene ved slaveanleggene, spesielt i Dunderdalen, over Saltfjellet og videre nordover, førte til store tap av menneskeliv (omtrent 2 000 menn), der 1 000 – 1 500 ble gravlagt langs banestrekningen. Av de 16 000 krigsfangene mellom Mo i Rana og Fauske, var 12 000 syke og forsultne, og arbeidet som ble gjennomført ble redusert av den grunn. Den Tyske byggeledelsen hadde dessuten skrevet at en tredjedel av fangene var syke, en tredjedel bare delvis arbeidsdyktige på grunn av sult, og bare den siste tredjedelen var arbeidsdyktige i februar 1945. Totalt sørget Tyskerne for å bygge 450 km på Nordlandsbanen, samt Sørlandsbanen, og som nevn tidligere, med svært høyt tempo. Etter krigen var NSB nødt for å bygge om mye av det tyskerne hadde gjennomført på Nordlandsbanen, spesielt strekningen mellom Storforshei og Lønsdal i årene 1945 – 1947. Vannomløp måtte rives og bygges om igjen, grøfter var helt forsømt, samt fjellkjæringer og sikring av jordskråninger var også ignorert. Tunneler var heller ikke sikret med skikkelig utstøpning, og det manglet plattformer på flere jernbanestasjoner (studieselsapets årsmøte. Se kildeliste).
+    </div>
+    <section>
+        <div class="img-container" style="width: 600px; margin: 0 0 50px 0;" >
+            <img src="./reklameboken_1.png" alt="Can't load">
+            <div class="img-text">
+                Ukjent forfatter (1953). Reklameboken for jernbanen i Nord-Norge. Nasjonalbiblioteket. (se kildeliste) «Tyskerne forlanger at banen skal være over Saltfjellet imorgen og på Fauske om 14 dager» ble det ironisk sagt blant jernbanearbeiderne.
+            </div>
+        </div>
+    </section>
+    <section>
+        <div class="img-container" style="width: 600px; margin: 0 0 50px 0;" >
+            <img src="./sovjetisk_krigsfange.jpg" alt="Can't load">
+            <div class="img-text">
+            </div>
+        </div>
+    </section>
+    <section>
+        <div class="text-container">
+            Vi kan raskt starte å trekke linjer mellom det dårlige jernbanearbeidet og de dårlige arbeidsforholdene som fant sted i krigstiden. Mange av strekningene nord for Mosjøen ble forsert av Tyskerne, og sammen med forsultne og lite arbeidsdyktige krigsfanger, resulterte dette til dårlig kvalitet på blant annet banelegemet. Krigsfangene levde tett i brakkene, og mange døde på grunn av tvangsarbeidet, sult og henrettelser. Til tross for dette hevdes det at avstanden Mosjøen til Mo i Rana at «banelegemets kvalitet ble slett ikke dårlig» på grunn av at arbeidet for det meste ble ledet av nordmenn (Reklameboken for jernbanen i Nord-Norge (1953). Se kildeliste.).
+        </div>
+        <div class="img-container" style="width: 300px;">
+            <img src="./fangeleirer.jpg" alt="Can't load">
+            <div class="img-text">
+                Kart over fangeleirer. «Leirene lå som perler på en snor langs nordlandsbanen.»
+            </div>
+        </div>
+    </section>
+    <div class="text-section">
+        <h2>TL;DR - Hvordan var det å jobbe på Nordlandsbanen?</h2>
+        I konklusjon har arbeidsforholdene på Nordlandsbanen variert i stor grad – Fra forsert tvangsarbeid under den tyske okkupasjonen, til et mer ordentlig arbeid etter 2. Verdenskrig der menneskerettighetene ikke ble brutt. Min oldefar, Øistein Orre Møller, har jobbet på Nordlandsbanen som blant annet skytebas, og han bodde for det meste i brakkebyer sammen med andre jernbanefolk. Han dro hjem til mormor samt familien i ferier og når han kunne, men fikk ikke se dem ofte, noe som dessverre var en realitet for mange jernbanearbeidere. Arbeidsdagene var lange og tunge, og arbeidet gikk trolig ut på ombygging og sikring av jernbanen, men også legging av skinner og tverliggende sviller til tider. I dag kan vi takke Øistein og resten av jernbanefolket for innsatsen deres i å bygge det Norge vi kjenner til i dag - en skinne om gangen :)
+    </div>
+    <section>
+        <div class="img-container" style="width: 600px; margin: 0 0 50px 0;" >
+            <img src="./nordlandsbanen_dikt.png" alt="Can't load">
+            <div class="img-text">
+                Hentet fra Reklameboken for jernbanen i Nord-Norge (1953).
+            </div>
+        </div>
+    </section>
+    <div class="source-section">
+        <div class="text-container">
+            <h3>Kildeliste</h3>
+            <ul>
+                <li><a href="https://snl.no/Nordlandsbanen" target="_blank" rel="noreferrer">snl.no – Nordlandsbanen (oppdatert: 23. desember 2022)</a></li>
+                <li><a href="https://oda.oslomet.no/oda-xmlui/bitstream/handle/20.500.12199/2265/2009-101.pdf?sequence=1&isAllowed=y" target="_blank" rel="noreferrer">oda.oslomet.no – Befolkningsutvikling på Smøla og Vega ()</a></li>
+                <li><a href="https://www.banenor.no/reisende/Banene/Nordlandsbanen/" target="_blank" rel="noreferrer">banenor.no – Nordlandsbanen ()</a></li>
+                <li><a href="https://www.klartale.no/tema/2012/10/20/de-ble-tvunget-i-arbeid/" target="_blank" rel="noreferrer">Cecilie Bakken Høstmark (20/10/2012 07:00) De ble tvunget i arbeid. Klartale.no</a></li>
+                <li><a href="https://www.riksantikvaren.no/prosjekt-nordlandsbanen/#:~:text=I%20leirene%2C%20som%20var%20fordelt,mens%20resten%20var%20fra%20Sovjetunionen" target="_blank" rel="noreferrer">riksantikvaren.no – Prosjekt Nordlandsbanen ()</a></li>
+                <li><a href="https://www.youtube.com/watch?v=yvHSShayAac&ab_channel=BarentsStories" target="_blank" rel="noreferrer">YouTube.com – Prosjekt Nordlandsbanen ()</a></li>
+                <li><a href="https://snl.no/Operasjon_asfalt" target="_blank" rel="noreferrer">Snl.no – Operasjon Asfalt ()</a></li>
+                <li><a href="https://lokalhistoriewiki.no/wiki/Nordlandsbanen_i_fred_og_krig" target="_blank" rel="noreferrer">lokalhistoriewiki.no – Nordlandsbanen i fred og krig ()</a></li>
+            </ul>
+        </div>
+        <div class="text-container">
+            <h3>Kilder fra nasjonalbiblioteket</h3>
+            <ul>
+                <li><a href="https://www.nb.no/items/URN:NBN:no-nb_digibok_2012060724014" target="_blank" rel="noreferrer">Ingvar Andersen (1986). Vegas Historie / Del 4 - Lokal Kultur / Etterkrigstida. Nasjonalbiblioteket.</a></li>
+                <li><a href="https://www.nb.no/items/a0888c0dfe3c8b6f212785c97224e4a5?page=27&searchText=nordlandsbanen" target="_blank" rel="noreferrer">Ukjent forfatter (1953). Reklameboken for jernbanen i Nord-Norge. Nasjonalbiblioteket.</a></li>
+                <li><a href="https://www.nb.no/items/a6a46567ffaf27fedad2f011b2b7581d?page=73&searchText=nordlandsbanen" target="_blank" rel="noreferrer">Studieselskapets For Nord-Norsk Næringsliv (1955). 8 foredrag fra Studieselskapets årsmøte 1955. Nasjonalbiblioteket.</a></li>
+                <li><a href="https://www.nb.no/items/f605e710ca954916fa7d6277f37c43f6?page=27&searchText=nordlandsbanen" target="_blank" rel="noreferrer">Arvild Ellingsve  (1995). Nordlandsbanen Krigshistorie. Nasjonalbiblioteket.</a></li>
+                <li><a href="https://www.nb.no/items/4dbf6e75f563185a504a2b3e43185d84?page=0&searchText=nordlandsbanen" target="_blank" rel="noreferrer">Kaare Wilhelmsen (1977). Jernbanefolk forteller. Nasjonalbiblioteket.</a></li>
+                <li><a href="https://www.nb.no/items/36f98a8bc2bfe8fcc93c2ab9a8312248?page=0&searchText=nordlandsbanen" target="_blank" rel="noreferrer">Nordlandsposten (desember 1947) Forside. Nasjonalbiblioteket.</a></li>
+            </ul>
+        </div>
+    </div>
+</div>
+
+
+
+<style>
+    h1 {
+        margin: 0;
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        color: white;
+        font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
+        background-color: rgba(0, 0, 0, 0.5);
+    }
+
+    h2 {
+        margin: 0;
+    }
+
+    p {
+        margin: 0 0 50px 0;
+    }
+
+    section {
+        margin: 0;
+        padding: 0;
+        gap: 50px;
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+        /* background-color: rgba(255, 200, 200, 1); */
+    }
+
+    .text-section {
+        width: 700px;
+        margin: 50px auto;
+        /* background-color: rgba(200, 255, 200, 1); */
+    }
+
+    .content {
+        width: 100%;
+        height: 100%;
+        display: flex;
+        flex-direction: column;
+        background-color: rgb(235, 235, 235);
+    }
+
+    .top-container {
+        margin: 0 0 50px 0;
+        padding: 0;
+        position: relative;
+        text-align: center;
+        /* background-color: rgba(200, 255, 200, 1); */
+    }
+
+    .full-img {
+        width: 100%;
+        max-width: 1600px;
+        box-shadow: 0 0 64px rgba(0, 0, 0, 0.9);
+    }
+
+    .text-container {
+        width: 500px;
+        /* background-color: rgba(200, 200, 255, 1); */
+    }
+
+    .img-container {
+        margin-top: 64px;
+        padding: 25px;
+        box-shadow: 0 0 16px rgba(0, 0, 0, 0.5);
+        display: flex;
+        flex-direction: column;
+        align-self:flex-start;
+        background-color: rgba(255, 255, 255, 1);
+    }
+
+    .img-text {
+        width: fit-content;
+        max-width: 100%;
+        font-size: medium;
+        font-style: italic;
+        /* background-color: rgba(200, 200, 255, 1); */
+    }
+
+    .video-section {
+        margin: 50px auto;
+        text-align: center;
+        background-color: rgba(255, 125, 125, 1);
+    }
+
+    .video-box {
+        width: 900px;
+        /*width: 900px;*/
+        height: 450px;
+        /*height: 562.50px;*/
+    }
+
+    .source-section {
+        margin: 50px 0 0 0;
+        padding: 0 0 100px 0;
+        gap: 50px;
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+        color: rgba(255, 255, 255, 1);
+        background-color: rgba(0, 0, 0, 1);
+    }
+</style>
